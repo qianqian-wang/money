bd77cb7a9439a59fb6567651e06290092d65efd2
Queen <qianqianwang0112@163.com>
2021-11-08 17:18:17 +0800

    拆分组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" placeholder="在这里添加备注" />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  padding: 0 16px;
+  display: flex;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+  }
+}
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="types">
+    <li>支出</li>
+    <li class="selected">收入</li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.types {
+  background: #c4c4c4;
+  display: flex;
+  // text-align: center;
+  li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    &.selected::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+</style>
