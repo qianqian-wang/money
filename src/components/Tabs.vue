b47a52490c069208c733d8c2982f123ef76cce20
Queen <qianqianwang0112@163.com>
2021-11-22 17:05:47 +0800

    初始化统计

---
+++ src/components/Tabs.vue
@@ -0,0 +1,68 @@
+<template>
+  <ul class="types">
+    <li
+      :class="liClass(item)"
+      @click="select(item)"
+      v-for="item in dataSource"
+      :key="item.value"
+    >
+      {{ item.text }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+
+type dataSourceItem = {
+  text: string;
+  value: string;
+};
+@Component
+export default class Tabs extends Vue {
+  @Prop(String)
+  readonly value!: string;
+
+  @Prop({ required: true, type: Array })
+  dataSource!: dataSourceItem[];
+
+  @Prop(String)
+  classPrefix?: string;
+
+  liClass(item: dataSourceItem) {
+    return {
+      [this.classPrefix + "-tabs-item"]: this.classPrefix,
+      selected: item.value === this.value,
+    };
+  }
+  select(item: dataSourceItem) {
+    this.$emit("update:value", item.value);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.types {
+  background: #c4c4c4;
+  display: flex;
+  // text-align: center;
+  li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    &.selected::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,9 +1,49 @@
 <template>
   <div>
-    <Layout>sasa</Layout>
+    <Layout>
+      <Tabs :dataSource="typeList" classPrefix="type" :value.sync="type" />
+      <Tabs
+        :dataSource="intervalList"
+        classPrefix="interval"
+        :value.sync="interval"
+      />
+      {{ type }}
+      <br />
+      {{ interval }}
+    </Layout>
   </div>
 </template>
 
 <script lang="ts">
-export default {};
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import Tabs from "@/components/Tabs.vue";
+
+@Component({
+  components: { Tabs },
+})
+export default class Statistics extends Vue {
+  type = "-";
+  interval = "day";
+  typeList = [
+    { text: "收入", value: "+" },
+    { text: "支出", value: "-" },
+  ];
+  intervalList = [
+    { text: "周", value: "week" },
+    { text: "月", value: "mouth" },
+    { text: "日", value: "day" },
+  ];
+}
 </script>
+<style scoped lang="scss">
+::v-deep .type-tabs-item {
+  background: white;
+  &.selected {
+    background: #c4c4c4;
+    &::after {
+      display: none;
+    }
+  }
+}
+</style>
